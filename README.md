681d005b47f402a4318a78cd75965d394b627baa
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2019-10-08 14:05:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## tolong di install
+yarn add bcryptjs body-parser express gravatar jsonwebtoken mongoose passport passport-jwt validator
+
+npm install -D nodemon
+
+## belum di atlas masih pake lokal
+# backend
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const {addUser,login,getAuth} = require ('../controller/user')
+
+router.post("/register",addUser);
+router.post("/login",login);
+router.get("/me",getAuth);
+
+
+module.exports = router;
